d329efdbb4da4f856b578390561d0612cbf245bb
vishnu.pp <vishnu.pp@acodez.co.in>
2023-02-16 11:11:45 +0530

    root updated

--- App.jsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import Home from "./src/pages";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(<Home />);
